8e7d37aaa8bd19e0eace67c086f93d0a02bf804f
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2024-09-04 23:50:37 -0300

    Divide code into classes and packages and turn more compatible

---
+++ app/src/main/java/com/prisma/prismavi/camera/CameraManeger.kt
@@ -0,0 +1,14 @@
+package com.prisma.prismavi.camera
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.prisma.prismavi.preview.CameraPreview
+
+class CameraManager(private val activity: ComponentActivity) {
+
+    fun startCamera() {
+        activity.setContent {
+            CameraPreview()
+        }
+    }
+}
