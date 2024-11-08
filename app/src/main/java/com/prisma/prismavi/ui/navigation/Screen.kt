5375c8c14dcd1e70132b46fb05d96cca6ee407ff
Prisma-VI <prismaviorg@gmail.com>
2024-11-08 19:49:36 -0300

    Punctual changes

--- app/src/main/java/com/prisma/prismavi/ui/navigation/Screen.kt
+++ app/src/main/java/com/prisma/prismavi/ui/navigation/Screen.kt
@@ -3,6 +3,8 @@ package com.prisma.prismavi.ui.navigation
 sealed class Screen {
     object Splash : Screen()
     object Camera : Screen()
+    
+//TODO
 //  object Settings : Screen() Exemplo de uma nova tela
 
 }
