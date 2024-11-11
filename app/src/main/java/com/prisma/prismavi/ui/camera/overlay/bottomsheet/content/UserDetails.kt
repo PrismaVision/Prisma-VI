2db2dd404bad4d1071c9c28b74d59903a934811a
Prisma-VI <samuel.soares.cunha64@gmail.com>
2024-11-11 20:43:10 -0300

    Add first row

--- app/src/main/java/com/prisma/prismavi/ui/camera/overlay/bottomsheet/content/UserDetails.kt
+++ app/src/main/java/com/prisma/prismavi/ui/camera/overlay/bottomsheet/content/UserDetails.kt
@@ -1,18 +1,54 @@
 package com.prisma.prismavi.ui.camera.overlay.bottomsheet.content
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun UserDetails(){
-    Text(
-        text = "UserDetails",
-        fontSize = 16.sp,
+    Column(
         modifier = Modifier
-            .padding(bottom = 8.dp)
-    )
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.Start
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(90.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color.Gray.copy(alpha = 0.3f))
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+
+
+        ){
+            Text(
+                text = "{Tittle variable}",
+                fontSize = 20.sp,
+                modifier = Modifier
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun UserDetailsPreview(){
+    UserDetails()
 }
