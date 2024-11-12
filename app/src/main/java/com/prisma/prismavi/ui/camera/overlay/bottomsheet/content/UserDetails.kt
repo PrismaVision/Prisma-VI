fe604da9c970815cc178587990bed4e27d624330
Prisma-VI <samuel.soares.cunha64@gmail.com>
2024-11-12 19:40:14 -0300

    Finish UserDetails structure

--- app/src/main/java/com/prisma/prismavi/ui/camera/overlay/bottomsheet/content/UserDetails.kt
+++ app/src/main/java/com/prisma/prismavi/ui/camera/overlay/bottomsheet/content/UserDetails.kt
@@ -1,7 +1,7 @@
 package com.prisma.prismavi.ui.camera.overlay.bottomsheet.content
 
-import android.widget.Button
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -26,6 +26,8 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun UserDetails(){
+
+    val colorList = listOf("#000000","#000001","#000011")
 
     var palleteList = listOf("Pallete1","Pallete2","Pallete3")
 
@@ -110,6 +112,64 @@ fun UserDetails(){
                                 imageVector = Icons.Sharp.Add,
                                 contentDescription = "Add",
                             )
+                        }
+                    }
+                }
+            }
+        }
+        Row(){
+            Column {
+                Text(
+                    text = "{Tittle variable}",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(
+                        top = 8.dp,
+                        bottom = 8.dp
+                    )
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(15.dp))
+                        .background(Color.Gray.copy(alpha = 0.3f))
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Column {
+                        for (color in colorList)
+                        {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clip(RoundedCornerShape(11.dp))
+                                    .background(Color.Gray.copy(alpha = 0.3f))
+                                    .padding(16.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Box(
+                                        modifier = Modifier
+                                            .size(45.dp)
+                                            .clip(RoundedCornerShape(10.dp))
+                                            .background(parseColor(color))
+                                            .padding(10.dp)
+                                    )
+
+                                    Text(
+                                        text = color,
+                                        fontSize = 14.sp,
+                                        modifier = Modifier
+                                            .padding(start = 4.dp)
+                                    )
+                                }
+                            }
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .height(15.dp)
+                            ){}
                         }
                     }
                 }
