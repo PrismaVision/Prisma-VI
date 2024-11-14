cf12fbfe0c8c4046cfb336341dc56c35d8939d31
c4ioba <caiovictorcvm10@gmail.com>
2024-11-13 23:57:12 -0300

    Make bottomsheet responsive

--- app/src/main/java/com/prisma/prismavi/ui/camera/overlay/bottomsheet/content/ColorDetails.kt
+++ app/src/main/java/com/prisma/prismavi/ui/camera/overlay/bottomsheet/content/ColorDetails.kt
@@ -2,12 +2,19 @@ package com.prisma.prismavi.ui.camera.overlay.bottomsheet.content
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.background
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -16,12 +23,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.rememberScrollState
 
 fun parseColor(colorString: String): Color {
     val colorLong = colorString.removePrefix("#").toLong(16)
@@ -38,128 +40,110 @@ val colorRyb: String = "RYB: (0%, 0%, 0%)"
 val descriptionText: String = "A cor $colorTemperatureName incorpora uma tonalidade intensa e vibrante que evoca sensações de calor e energia. Ele irradia uma essência ousada e ardente, que lembra um pôr do sol escaldante ou brasas brilhantes. Este tom cativante capta a atenção e desperta a paixão, tornando-o perfeito para designs que visam inspirar excitação e vitalidade.\""
 val colorTerminology: String = "Primária"
 val colorMatch1: String = "#000000"
-val colorMatch2: String = "#111111"
+val colorMatch2: String = "#000000"
+
 
 @Composable
 fun ColorDetails() {
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+    val sheetHeight = screenHeight * 0.80f
+
     Column(
         modifier = Modifier
+            .height(sheetHeight)
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()), // Habilita rolagem vertical
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        // Seção com o nome da cor e o quadrado de cor
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(150.dp)
-        ){
-
-        Box(
-            modifier = Modifier
-                .size(150.dp)
-                .clip(RoundedCornerShape(20.dp))
-                .background(formatedHexCode)
-        )
-
-        Column(
-            modifier = Modifier
-                .padding(start = 15.dp)
-                .fillMaxWidth()
+                .height(screenHeight * 0.2f)
+                .padding(bottom = 8.dp)
         ) {
-            Text(
-                text = colorName,
-                fontSize = 16.sp,
+            Box(
                 modifier = Modifier
-                    .padding(bottom = 8.dp) // Espaçamento abaixo do título
+                    .size(screenWidth * 0.35f)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(parseColor("#$hexCode"))
             )
 
             Column(
-                verticalArrangement = Arrangement.Center,
                 modifier = Modifier
+                    .padding(start = 15.dp)
                     .fillMaxWidth()
-                    .fillMaxHeight()
-                    .clip(RoundedCornerShape(15.dp)) // Define a forma arredondada dos cantos
-                    .background(Color.Gray.copy(alpha = 0.3f))
-                    .padding(16.dp)
-                    // Espaçamento interno
-
             ) {
-                Row(){
-                    Text(
-                        text = colorHex,
-                        fontSize = 14.sp // Tamanho padrão
-                    )
-                }
-                Row(){
-                    Text(
-                        text = colorRgb,
-                        fontSize = 14.sp// Tamanho menor do texto
-                    )
-                }
-                Row(){
-                    Text(
-                        text = colorRyb,
-                        fontSize = 14.sp // Tamanho menor do texto
-                    )
-                }
+                Text(
+                    text = colorName,
+                    fontSize = (screenWidth * 0.04f).value.sp,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
 
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .clip(RoundedCornerShape(15.dp))
+                        .background(Color.Gray.copy(alpha = 0.3f))
+                        .padding(16.dp)
+                ) {
+                    Text(text = colorHex, fontSize = 14.sp)
+                    Text(text = colorRgb, fontSize = 14.sp)
+                    Text(text = colorRyb, fontSize = 14.sp)
+                }
             }
         }
-    }
 
+        // Seção de temperatura e terminologia
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 8.dp) // Preenchimento interno acima da coluna
+                .padding(top = 8.dp)
         ) {
-            // Linha contendo "Color Temperature" e "Color Match"
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                // Coluna para "Color Temperature" e "Color Terminology"
                 Column(
                     modifier = Modifier
-                        .weight(0.9f) // Faz a coluna ocupar metade do espaço disponível
-                        .padding(end = 8.dp) // Espaçamento à direita
+                        .weight(0.9f)
+                        .padding(end = 8.dp)
                 ) {
                     Text(
                         text = "Temperatura da Cor",
-                        fontSize = 16.sp,
-                        modifier = Modifier
-                            .padding(bottom = 8.dp)
+                        fontSize = (screenWidth * 0.04f).value.sp,
+                        modifier = Modifier.padding(bottom = 8.dp)
                     )
 
-                    // Box para "Color Temperature" (sua temperatura de cor)
                     Box(
                         modifier = Modifier
-                            .height(70.dp) // Altura do Box
+                            .height(screenHeight * 0.1f)
                             .fillMaxWidth()
-                            .clip(RoundedCornerShape(15.dp)) // Define a forma arredondada dos cantos
-                            .background(Color.Gray.copy(alpha = 0.3f)) // Aplica o background cinza
-                            .padding(16.dp) // Espaçamento interno
+                            .clip(RoundedCornerShape(15.dp))
+                            .background(Color.Gray.copy(alpha = 0.3f))
+                            .padding(16.dp)
                     ) {
                         Text(
                             text = colorTemperatureName,
-                            fontSize = 16.sp,
-                            modifier = Modifier.align(Alignment.CenterStart) // Alinha o texto à esquerda
+                            fontSize = (screenWidth * 0.04f).value.sp,
+                            modifier = Modifier.align(Alignment.CenterStart)
                         )
                     }
 
-                    // Texto "Color Terminology"
                     Text(
                         text = "Terminologia da Cor",
-                        fontSize = 16.sp,
-                        modifier = Modifier.padding(
-                            top = 8.dp,
-                            bottom = 8.dp
-                        )
+                        fontSize = (screenWidth * 0.04f).value.sp,
+                        modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
                     )
 
                     Box(
                         modifier = Modifier
-                            .height(70.dp)
+                            .height(screenHeight * 0.1f)
                             .fillMaxWidth()
                             .clip(RoundedCornerShape(15.dp))
                             .background(Color.Gray.copy(alpha = 0.3f))
@@ -167,9 +151,8 @@ fun ColorDetails() {
                     ) {
                         Text(
                             text = colorTerminology,
-                            fontSize = 16.sp, // Tamanho padrão
-                            modifier = Modifier
-                                .align(Alignment.CenterStart)
+                            fontSize = (screenWidth * 0.04f).value.sp,
+                            modifier = Modifier.align(Alignment.CenterStart)
                         )
                     }
                 }
@@ -177,15 +160,13 @@ fun ColorDetails() {
                 Column(
                     modifier = Modifier
                         .weight(1f)
-                        .height(203.dp)
+                        .height(screenHeight * 0.28f)
                         .padding(start = 10.dp)
                 ) {
                     Text(
                         text = "Cores Correspondentes",
-                        fontSize = 16.sp,
-                        modifier = Modifier
-                            .padding(bottom = 8.dp)
-                            .align(Alignment.Start)
+                        fontSize = (screenWidth * 0.04f).value.sp,
+                        modifier = Modifier.padding(bottom = 8.dp)
                     )
 
                     Box(
@@ -195,9 +176,7 @@ fun ColorDetails() {
                             .padding(10.dp)
                     ) {
                         Column(
-                            modifier = Modifier.fillMaxWidth()
-                                .padding(10.dp)
-                                .fillMaxHeight(),
+                            modifier = Modifier.fillMaxWidth().padding(10.dp).fillMaxHeight(),
                             verticalArrangement = Arrangement.Center
                         ) {
                             Row(
@@ -208,19 +187,18 @@ fun ColorDetails() {
                                     modifier = Modifier
                                         .size(45.dp)
                                         .clip(RoundedCornerShape(10.dp))
-                                        .background(formatedHexCode)
+                                        .background(parseColor(colorMatch1))
                                         .padding(10.dp)
                                 )
 
                                 Text(
-                                    text = colorMatch1, 
+                                    text = colorMatch1,
                                     fontSize = 14.sp,
-                                    modifier = Modifier
-                                        .padding(start = 6.dp)
+                                    modifier = Modifier.padding(start = 6.dp)
                                 )
                             }
 
-                            Spacer(modifier = Modifier.height(25.dp))
+                            Spacer(modifier = Modifier.height(20.dp))
 
                             Row(
                                 modifier = Modifier.fillMaxWidth(),
@@ -230,48 +208,44 @@ fun ColorDetails() {
                                     modifier = Modifier
                                         .size(45.dp)
                                         .clip(RoundedCornerShape(10.dp))
-                                        .background(formatedHexCode)
+                                        .background(parseColor(colorMatch2))
                                         .padding(10.dp)
                                 )
 
                                 Text(
                                     text = colorMatch2,
                                     fontSize = 14.sp,
-                                    modifier = Modifier
-                                        .padding(start = 6.dp)
+                                    modifier = Modifier.padding(start = 6.dp)
                                 )
                             }
                         }
                     }
                 }
-
             }
         }
 
+        // Seção de descrição
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 8.dp), // Preenchimento interno acima da coluna
+                .padding(top = 8.dp)
         ) {
-            // Texto acima da Column de detalhes
             Text(
                 text = "Descrição",
-                fontSize = 16.sp,
-                modifier = Modifier.padding(bottom = 8.dp) // Espaçamento abaixo do texto
+                fontSize = (screenWidth * 0.04f).value.sp,
+                modifier = Modifier.padding(bottom = 8.dp)
             )
 
             Box(
                 modifier = Modifier
-                    .clip(RoundedCornerShape(15.dp)) // Define a forma arredondada dos cantos
-                    .background(Color.Gray.copy(alpha = 0.3f)) // Aplica o background cinza
-                    .padding(16.dp) // Espaçamento interno
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(Color.Gray.copy(alpha = 0.3f))
+                    .padding(16.dp)
             ) {
-                // Colocando o texto alinhado à esquerda e centralizado verticalmente
                 Text(
                     text = descriptionText,
-                    fontSize = 16.sp, // Tamanho padrão
-                    modifier = Modifier
-                        .align(Alignment.CenterStart) // Alinha o texto à esquerda, mas centraliza verticalmente
+                    fontSize = (screenWidth * 0.04f).value.sp,
+                    modifier = Modifier.align(Alignment.CenterStart)
                 )
             }
         }
