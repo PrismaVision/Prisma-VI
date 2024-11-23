458b8aa5416baf9c817101abd2a9b69e9b6d9431
Prisma-VI <samuel.soares.cunha64@gmail.com>
2024-11-23 17:56:24 -0300

    Update bottomsheet

--- app/src/main/java/com/prisma/prismavi/ui/camera/overlay/bottomsheet/BottomSheetPreview.kt
+++ app/src/main/java/com/prisma/prismavi/ui/camera/overlay/bottomsheet/BottomSheetPreview.kt
@@ -1,20 +1,17 @@
 package com.prisma.prismavi.ui.camera.overlay.bottomsheet
 
-import androidx.compose.foundation.background
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.tooling.preview.Preview
+import com.prisma.prismavi.ui.camera.overlay.bottomsheet.content.BottomSheetBackButton
 import com.prisma.prismavi.ui.navigation.bottomsheet.BottomSheetViewManager
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,7 +20,7 @@ fun BottomSheetPreview(
 ) {
 
     lateinit var bottomSheetViewManager: BottomSheetViewManager
-    val bottomSheetHeight: Dp by remember { mutableStateOf(105.dp) }
+    val bottomSheetHeight: Dp by remember { mutableStateOf(55.dp) }
 
     val scaffoldState = rememberBottomSheetScaffoldState()
 //  val scope = rememberCoroutineScope()
@@ -36,6 +33,7 @@ fun BottomSheetPreview(
         sheetPeekHeight = bottomSheetHeight,
         sheetContent = {
             bottomSheetViewManager = BottomSheetViewManager()
+            BottomSheetBackButton(bottomSheetViewManager = bottomSheetViewManager)
             bottomSheetViewManager.RenderContent()
         }
     ) {
